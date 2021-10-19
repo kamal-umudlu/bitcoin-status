ac8687a645c9f3617aae7734e004cb5275b536bc
kamal-umudlu <kamalumudlu17@gmail.com>
2021-10-19 17:21:02 +0400

    Finished 30 days max, min bitcoin rate feature and modified pom to make jar file runnable

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,15 +5,28 @@ import okhttp3.Response;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Map;
 import java.util.Scanner;
 
 public class App {
     public static final String QUIT = "QUIT";
     public static final String HELP = "HELP";
     public static final String BITCOIN_CURRENT_PRICE_ENDPOINT = "https://api.coindesk.com/v1/bpi/currentprice/%s.json";
+    public static final String BITCOIN_PAST_PRICES_ENDPOINT = "https://api.coindesk.com/v1/bpi/historical/close.json?start=%s&end=%s&currency=%s";
+
     private OkHttpClient client = new OkHttpClient();
-    private float currentPrice;
+    private BigDecimal currentPrice;
+    private BigDecimal minPrice;
+    private BigDecimal maxPrice;
+
+    public App() {
+        this.currentPrice = null;
+        this.minPrice = null;
+        this.maxPrice = null;
+    }
 
     public static void main(String[] args) {
         App app = new App();
@@ -44,6 +57,17 @@ public class App {
                         continue;
                     }
                     parseCurrentBitcoinPrice(responseBody, commandLine);
+                    LocalDate currentDate = LocalDate.now();
+                    url = String.format(BITCOIN_PAST_PRICES_ENDPOINT, currentDate.minusDays(30), currentDate, commandLine);
+                    request = new Request.Builder().url(url).build();
+                    response = client.newCall(request).execute();
+                    responseBody = response.body().string();
+                    if (response.code() == ErrorMessages.NOT_FOUND.getErrorCode()) {
+                        System.out.println(responseBody);
+                        System.out.println("Currency code not found in coindesk.com. Please enter another currency: ");
+                        continue;
+                    }
+                    parseBitcoinPastPrice(responseBody, commandLine);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -53,14 +77,27 @@ public class App {
     }
 
     private void help() {
-        System.out.println("These are commands that you can use: ");
+        System.out.println("These are the commands that you can use: ");
         System.out.println("1. Currency codes: 'USD', 'EUR', 'GBP', etc.. ");
         System.out.println("2. Type 'QUIT' to exit from program");
     }
 
     private void parseCurrentBitcoinPrice(String str, String currency) {
         JSONObject jsonObject = new JSONObject(str);
-        currentPrice = jsonObject.getJSONObject("bpi").getJSONObject(currency).getFloat("rate_float");
-        System.out.println(LocalDateTime.now() + " - Current bitcoin rate = " + currentPrice + " " + currency + " \n");
+        currentPrice = jsonObject.getJSONObject("bpi").getJSONObject(currency).getBigDecimal("rate_float");
+        System.out.println(LocalDateTime.now() + " - Current bitcoin rate = " + currentPrice.toString() + " " + currency + " \n");
+    }
+
+    private void parseBitcoinPastPrice(String str, String currency) {
+        JSONObject jsonObject = new JSONObject(str);
+        BigDecimal minRate = null, maxRate = null;
+        for (Map.Entry<String, Object> entry : jsonObject.getJSONObject("bpi").toMap().entrySet()) {
+            minRate = minRate == null ? (BigDecimal) entry.getValue() : minRate.min((BigDecimal) entry.getValue());
+            maxRate = maxRate == null ? (BigDecimal) entry.getValue() : maxRate.max((BigDecimal) entry.getValue());
+        }
+        System.out.println("Minimum bitcoin rate in last 30 days = " + minRate.toString() + " " + currency + "\n");
+        System.out.println("Maximum bitcoin rate in last 30 days = " + maxRate.toString() + " " + currency + "\n");
+        this.maxPrice = maxRate;
+        this.minPrice = minRate;
     }
 }
